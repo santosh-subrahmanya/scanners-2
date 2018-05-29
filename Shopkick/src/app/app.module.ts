1a106e4d40f17259adaedbc94a5e8a7b91ca0ad6
santosh-subrahmanya <santosh.subrahmanya@gmail.com>
2018-05-29 11:34:13 +0530

    New Project folder for Angular
    New Project folder for Angular

---
+++ Shopkick/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { AppLayoutComponent } from './_layout/app-layout/app-layout.component';
+import { SiteLayoutComponent } from './_layout/site-layout/site-layout.component';
+import { AppHeaderComponent } from './_layout/app-header/app-header.component';
+import { SiteHeaderComponent } from './_layout/site-header/site-header.component';
+import { SiteFooterComponent } from './_layout/site-footer/site-footer.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
+import { HttpClientModule } from "@angular/common/http";
+import { routing } from './app.routing';
+import { BarcodeComponent } from './barcode/barcode.component';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule, routing, HttpClientModule ],
+  declarations: [ AppComponent, HelloComponent, AppLayoutComponent, SiteLayoutComponent, AppHeaderComponent, SiteHeaderComponent, SiteFooterComponent, LoginComponent, DashboardComponent, HomeComponent, AboutComponent, RegisterComponent, ProfileComponent, BarcodeComponent ],
+  bootstrap:    [ AppComponent, BarcodeComponent ]
+})
+export class AppModule { }
